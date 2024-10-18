469947418490560c0d720f172a2c8831101eeef0
pegasus019 <parthchamp100@gmail.com>
2024-10-18 21:49:56 +0100

    Create vector.py

---
+++ vector.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+#initialize the vectors
+v=pd.array([1,2,3,4,5,6,7,8,9,10])
+print('v=',v)
+
+# Access the elements
+print('v[0] =', v[0])
+print('v[1] =', v[1])
+print('v[2] =', v[2])
+print('v[3] =', v[3])
+print('v[4] =', v[4])
+print('v[5] =', v[5])
+print('v[6] =', v[6])
+print('v[7] =', v[7])
+print('v[8] =', v[8])
+print('v[9] =', v[9])
+
+
+
