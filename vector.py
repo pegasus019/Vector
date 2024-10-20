80f1238004f38e9e3ced27d0a862c5ffefd74303
pegasus019 <parthchamp100@gmail.com>
2024-10-20 17:46:13 +0100

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.linear_model, sklearn.datasets # sklearn is an important package for much of the ML we will be doing
 
 #initialize the vectors
 v=pd.array([1,2,3,4,5,6,7,8,9,10])
@@ -40,5 +42,35 @@ print('A^T =\n', A.T, '\n')
 print('Matrix Multiplication')
 print('AB =\n', A.dot(B), '\n')
 
+# Create our arrays of training data
+# Note, because we are using `intercept_fit=True` (the default) in our linear model, we do not have to include the column of 1's
+# The model implicitly know we want to calculate theta_0
+X = np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9],[10]])
+y = np.array([[2.259000208],[2.089513864],[2.691697786],[4.335616639],[5.990895127],[5.715164414],[6.785837863],[7.833729282],[9.136204457],[8.322833045]])
+
+# Create linear regression object
+obj = sklearn.linear_model.LinearRegression(fit_intercept=True)
+
+# Train the model using the training sets
+obj.fit(X, y)
+
+# Can obtain out model coefficients from the model obj we have trained
+print('theta_0:',obj.intercept_)
+print('theta_1:',obj.coef_)
+
+# Plot our results
+plt.scatter(X, y,  color='black', label='y')
+plt.plot(X, obj.predict(X), color='blue', label='Linear Model')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend()
+plt.show()
+
+# Look at our evaluation metrics - how well have we fit the data?
+# Note: Usually we will be computing these on predictions based on our test data, but for this case we are testing how well our model fits the data we have.
+# The mean squared error loss
+print('Mean squared error loss: {:.4f}'.format(sklearn.metrics.mean_squared_error(y, obj.predict(X))))
+# The R2 score: 1 is perfect prediction
+print('R2 score: {:.4f}'.format(sklearn.metrics.r2_score(y, obj.predict(X))))
 
 
